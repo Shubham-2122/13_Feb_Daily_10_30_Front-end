b5d99128e37e544a6af543d96d852cac751547fd
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-04-07 11:27:15 +0530

    javascript

---
+++ Javscript/ex.js
@@ -0,0 +1,17 @@
+// console.log("hello External Javascript");
+
+// document.write("hello javascript")
+
+// BOM :- browser object model
+
+// msg use error
+// alert("pls required field")
+
+// confirm
+// confirm("Are you sure?")
+
+// promy
+// prompt("5 + 5 = ?")
+
+// print
+// print()
