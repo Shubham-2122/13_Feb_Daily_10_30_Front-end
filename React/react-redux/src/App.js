03ea81c453c2ad01d4b548181efa97b418200d88
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-07-05 13:21:46 +0530

    redux

---
+++ React/react-redux/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Count from './features/Counter/Count'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello this react + redux App</h1>
+      <Count />
+    </div>
+  )
+}
+
+export default App
---
+++ React/react-redux/src/features/Counter/Count.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, zero } from './countSlice'
+
+function Count() {
+
+    const count = useSelector((state)=>state.count.value)
+
+    console.log(count)
+
+    const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h1>Hello count</h1>
+      <h1>Count :- {count}</h1>
+      <button onClick={()=>dispatch(increment())}>Increment</button>
+      <button onClick={()=>dispatch(decrement())}>decrement</button>
+      <button onClick={()=>dispatch(zero())}>Zero</button>
+    </div>
+  )
+}
+
+export default Count
